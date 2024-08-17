eca9b191a6c105a53ef8b1a83503fe2a584ab764
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-08-17 11:22:21 -0700

    Solution 1 complete

--- solution1.py
+++ solution1.py
@@ -1,1 +1,30 @@
 # Write code for algorithm 1 below
+
+# MY SOLUTION 
+def do_math(n):
+    #base case
+    if n == 0:
+        return (0)
+    
+    # recursive case
+    else:
+        print(n)
+        return (do_math(n-1))
+
+print(do_math(5))
+
+# ACTIVITY SOLUTION
+# def count_down(n):
+
+#   #  Base case
+#   if n==0:
+#       return
+
+#   #  Recursive case
+#   else:
+#       print(n)
+#       count_down(n-1)
+
+# # test case
+# n=8
+# count_down(n)
