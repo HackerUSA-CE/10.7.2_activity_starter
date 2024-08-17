6e354ab60b809fa593afc0fedb039a872d57cc01
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-08-17 12:15:56 -0700

    Solution 5 complete

--- solution4.py
+++ solution4.py
@@ -1,6 +1,12 @@
 # Write code for algorithm 4 below
 
-def power(num):
+def power(a, b):
     # base case
+    if b == 0:
+        return (1)
     
-    
+    #recursive case
+    else:
+        return a * power(a, b - 1)
+
+print(power(2, 4))
