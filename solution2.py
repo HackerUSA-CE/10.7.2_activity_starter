2354461c11fe229f919bd7f8a1c95c56b93b3acd
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-08-17 11:32:55 -0700

    Solution 2 complete

--- solution2.py
+++ solution2.py
@@ -1,1 +1,16 @@
 # Write code for algorithm 2 below
+
+def natural_numbers(x,i):
+	# base case
+	if i > x:
+		return 
+	
+    # recursive case
+	else:
+		print(i)
+		return (natural_numbers( x, i + 1))
+
+natural_numbers(3, 1)
+
+
+# expected output: 1 2 3
