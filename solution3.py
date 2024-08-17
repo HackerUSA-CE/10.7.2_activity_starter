10e2606b4fcbeebfe24b019e604f2116a24f3f61
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-08-17 11:44:22 -0700

    Solution 3 complete

--- solution3.py
+++ solution3.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 3 below
+
+def find_fibonacci(num):
+    # base case
+    print("Num",num)
+    if num <= 1:
+        return num
+
+    #recursive case 
+    else:
+        return (find_fibonacci(num - 1) + find_fibonacci(num - 2))
+    
+print(find_fibonacci(10))
